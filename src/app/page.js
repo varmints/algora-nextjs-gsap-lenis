69dd9ea928494cae8891603f92f62dace6eb7622
Kamil <example@example.com>
2025-02-15 20:54:38 +0100

    add border radius to carousel
    add border radius to carousel

--- src/app/page.js
+++ src/app/page.js
@@ -486,6 +486,28 @@ export default function Home() {
                     : "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
               }}
             >
+              <svg className="flt_svg" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                  <filter id="flt_tag">
+                    <feGaussianBlur
+                      in="SourceGraphic"
+                      stdDeviation="20"
+                      result="blur"
+                    />
+                    <feColorMatrix
+                      in="blur"
+                      mode="matrix"
+                      values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                      result="flt_tag"
+                    />
+                    <feComposite
+                      in="SourceGraphic"
+                      in2="flt_tag"
+                      operator="atop"
+                    />
+                  </filter>
+                </defs>
+              </svg>
               <div className="project-bg">
                 <img src={item.bg} alt="" />
 
